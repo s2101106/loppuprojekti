fe5f1cb2c10a419abc0d10ce8fffe93d0d6cec8f
s2101106 <s2101106@edu.bc.fi>
2023-10-31 14:35:12 +0200

    Eka commit

---
+++ Loppupeli/Peli.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_CsLo;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Loppupeli
+{
+    internal class Peli
+    {
+        const int window_width = 640;
+        const int window_height = 420;
+        public Peli() 
+        { 
+        
+        }
+        public void Run()
+        {
+            Init();
+            GameLoop();
+        }
+        void Init()
+        {
+            Raylib.InitWindow(window_width, window_height, "LoppyPeli");
+            Raylib.SetTargetFPS(30);
+        }
+        void GameLoop()
+        {
+            while(Raylib.WindowShouldClose()==false)
+            {
+                DrawGame();
+            }
+            
+        }
+        void DrawGame()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Raylib.BLUE);
+            Raylib.DrawCircle(window_width / 2, window_height / 2, 20, Raylib.MAROON);
+            Raylib.DrawText(Raylib.TextFormat($"Score:500"), 550, 400, 20, Raylib.RED);
+            Raylib.EndDrawing();
+        }
+    }
+}
---
+++ Loppupeli/Program.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_CsLo;
+namespace Loppupeli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Peli game = new Peli();
+            game.Run();
+        }
+    }
+}
