ec54f6c213703e8e9d75662b091ded47df7a1952
jvuorre <vuorrejanne@gmail.com>
2023-12-12 13:41:06 +0200

    Updase

--- Loppupeli/Peli.cs
+++ Loppupeli/Peli.cs
@@ -7,14 +7,25 @@ using Raylib_CsLo;
 using static System.Formats.Asn1.AsnWriter;
 using TurboMapReader;
 using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Loppupeli
 {
     internal class Peli
     {
+        enum GameState
+        {
+            Start,
+            Play,
+            Pause,
+            Options,
+            DevMenu
+        }
+        GameState state;
         const int window_width = 640;
         const int window_height = 420;
         public TiledMap tiledKartta;
+        public TiledMap tiledPiikit;
         Player player;
         Texture playerImage;
         Texture enemyImage;
@@ -25,10 +36,12 @@ namespace Loppupeli
         float cameraY;
         Enemy enemy;
         Gem gem;
+        int GemCounter;
         Vector2 playerStart;
         Vector2 enemyStart;
         Vector2 gemStart;
         List<Rectangle> mapColliders=new List<Rectangle>();
+        List<Rectangle> mapPiikit=new List<Rectangle>();
         float collisionAmount;
         public Peli() 
         { 
@@ -42,10 +55,12 @@ namespace Loppupeli
         void Init()
         {
             Raylib.InitWindow(window_width, window_height, "LoppyPeli");
+            Raylib.SetExitKey(KeyboardKey.KEY_DELETE);
             playerImage = Raylib.LoadTexture("kuvat\\ufoRed.png");
             enemyImage = Raylib.LoadTexture("kuvat\\ufoRed.png");
             gemImage = Raylib.LoadTexture("kuvat\\loppuGemi.png");
             atlasImage = Raylib.LoadTexture("kuvat\\sheet.png");
+            //piikkiImage = Raylib.LoadTexture("kuvat\\sheet.png");
             float playerSpeed = 120;
             int playerSize =16;
             Vector2 playerStart=new Vector2(0, 80);
@@ -54,10 +69,11 @@ namespace Loppupeli
             player =new Player(playerStart, playerSpeed, playerSize,playerImage,Raylib.RED);
             enemy = new Enemy(enemyStart, new Vector2(1,0),playerSpeed,playerSize, enemyImage);
             gem = new Gem(gemStart, new Vector2(1, 0), playerSpeed, playerSize, gemImage);
+            GemCounter = 0;
             camera.target=player.transform.position;
             camera.zoom = 2.0f;
             tiledKartta=MapReader.LoadMapFromFile("kartta1.tmj");
-            tiledKartta.PrintToConsole();
+            //tiledPiikit = MapReader.LoadMapFromFile("kartta1.tmj");
             Raylib.SetTargetFPS(30);
         }
         void GameLoop()
@@ -65,9 +81,30 @@ namespace Loppupeli
             while(Raylib.WindowShouldClose()==false)
             {
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Raylib.BLACK);
-                UpdateGame();
-                DrawGame();
+                switch (state)
+                {
+                    case GameState.Play:
+                        Raylib.ClearBackground(Raylib.BLACK);
+                        UpdateGame();
+                        DrawGame();
+                        break;
+                    case GameState.Start:
+                        Raylib.ClearBackground(Raylib.BLACK);
+                        MainMenu();
+                        break;
+                    case GameState.Pause:
+                        Raylib.ClearBackground(Raylib.BLACK);
+                        PauseMenu();
+                        break;
+                    case GameState.DevMenu:
+                        Raylib.ClearBackground(Raylib.BLACK);
+                        DevMenu();
+                        break;
+                    case GameState.Options:
+                        Raylib.ClearBackground(Raylib.BLACK);
+                        OptionsMenu();
+                        break;
+                }
                 Raylib.EndDrawing();
 
             }
@@ -89,6 +126,19 @@ namespace Loppupeli
                     DrawTile(x, y, tileId);
                 }
             }
+            /*int map_width = tiledPiikit.layers[0].data.Length;
+            int rows = map_width / tiledKartta.width;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < tiledPiikit.width; col++)
+                {
+                    int tileId = tiledPiikit.layers[0].data[row * tiledPiikit.width + col];
+                    int x = col * tiledPiikit.tilewidth;
+                    int y = row * tiledPiikit.tileheight;
+                    tileId--;
+                    DrawPiikki(x, y, tileId);
+                }
+            }*/
             player.Draw();
             enemy.Draw();
             gem.Draw();
@@ -111,10 +161,18 @@ namespace Loppupeli
             //top
             Raylib.DrawRectangle(-30 +2, 130, 12, 4, Raylib.RED);
             Raylib.DrawCircle(window_width / 2, window_height / 2, 20, Raylib.MAROON);
-            Raylib.DrawText(Raylib.TextFormat($"Score:500"), 550, 400, 20, Raylib.RED);
+            Raylib.DrawText(Raylib.TextFormat($"Score:{GemCounter}"), player.transform.position.X+110, player.transform.position.Y+85, 20, Raylib.RED);
         }
         void UpdateGame()
         {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_ESCAPE))
+            {
+                state = GameState.Pause;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_P))
+            {
+                state = GameState.DevMenu;
+            }
             player.Update();
             enemy.Update();
             cameraX = player.transform.position.X - 100;
@@ -140,40 +198,55 @@ namespace Loppupeli
                     {
                         collisionAmount= (collider.x + 16)-player.transform.position.X;
                         player.transform.position.X+=collisionAmount;
-                        Console.WriteLine($"OSUIT liikkuessa vasemmalle!");
-                        Console.WriteLine(mapColliders.Count.ToString());
                     }
                     else if (Raylib.CheckCollisionRecs(collider, playerRRec))
                     {
                         collisionAmount = (collider.x - 16) - player.transform.position.X;
                         player.transform.position.X += collisionAmount;
-                        Console.WriteLine($"OSUIT liikkuessa oikealle! {collider.x} {player.transform.position.X} {collisionAmount}");
-                        Console.WriteLine(mapColliders.Count.ToString());
                     }
                     else if (Raylib.CheckCollisionRecs(collider, playerTRec))
                     {
                         collisionAmount = (collider.y + 16) - player.transform.position.Y;
                         player.transform.position.Y += collisionAmount;
-                        Console.WriteLine("OSUIT liikkuessa ylös!");
-                        Console.WriteLine(mapColliders.Count.ToString());
                     }
                     else if (Raylib.CheckCollisionRecs(collider, playerBRec))
                     {
                         player.isGround = true;
                         collisionAmount = (collider.y - 16) - player.transform.position.Y;
                         player.transform.position.Y += collisionAmount;
-                        Console.WriteLine("OSUIT liikkuessa alas!");
-                        Console.WriteLine(mapColliders.Count.ToString());
                     }
 
                 }
             }
             Rectangle playerRec = getRectangle(player.transform,player.collision);
-            Rectangle gemRec = getRectangle(gem.transform, gem.collision);
-            if(Raylib.CheckCollisionRecs(gemRec, playerRec))
+            /*if (mapPiikit.Count >= tiledPiikit.layers[0].data.Length)
             {
-                Console.WriteLine("keräsit timun!");
-                gem.active = false;
+                foreach (Rectangle collider in mapPiikit)
+                {
+                    if (Raylib.CheckCollisionRecs(collider, playerRec))
+                    {
+                        Console.WriteLine("Kuolit");
+                    }
+                }
+            }*/
+            if (gem.active == true)
+            {
+                Rectangle gemRec = getRectangle(gem.transform, gem.collision);
+                if(Raylib.CheckCollisionRecs(gemRec, playerRec))
+                {
+                    Console.WriteLine("keräsit timun!");
+                    GemCounter++;
+                    gem.active = false;
+                }
+            }
+            if (enemy.active == true)
+            {
+                Rectangle enemyRec = getRectangle(enemy.transform, enemy.collision);
+                if (Raylib.CheckCollisionRecs(enemyRec, playerRec))
+                {
+                    Console.WriteLine("kuolit!");
+                    enemy.active = false;
+                }
             }
         }
         void DrawTile(int x, int y, int tileId)
@@ -194,6 +267,24 @@ namespace Loppupeli
                 mapColliders.Add(getRect(x,y,16));
             }
         }
+        /*void DrawPiikki(int x, int y, int tileId)
+        {
+            int tilesPerRow = 272 / tiledPiikit.tilewidth;
+            //Console.WriteLine(tilesPerRow.ToString());
+            double rowf = Math.Floor((double)tileId / (double)tilesPerRow);
+
+            int row = Convert.ToInt32(rowf);
+            int column = tileId % tilesPerRow;
+
+            int u = column * tiledPiikit.tilewidth;
+            int v = row * tiledPiikit.tileheight;
+            Vector2 xy = new Vector2(x, y);
+            Raylib.DrawTextureRec(piikitImage, new Rectangle(u, v, tiledPiikit.tilewidth, tiledPiikit.tileheight), xy, Raylib.WHITE);
+            if (mapPiikit.Count != tiledPiikit.layers[0].data.Length && tileId != -1)
+            {
+                mapPiikit.Add(getRect(x, y, 16));
+            }
+        }*/
         Rectangle getRectangle(TransformComponent t, CollisionComponent c)
         {
             Rectangle r = new Rectangle(t.position.X,
@@ -226,6 +317,42 @@ namespace Loppupeli
         {
             Rectangle r = new Rectangle(x,y, size, size);
             return r;
+        }
+        void MainMenu()
+        {
+            Raylib.DrawText("Jumper",window_width/2-75, 30, 50, Raylib.RED);
+            if (RayGui.GuiButton(new Rectangle(window_width/2-50, 200, 100, 50), "Play"))
+            {
+                state = GameState.Play;
+            }
+        }
+        void PauseMenu()
+        {
+            Raylib.DrawText("Paused", window_width / 2 - 75, 30, 50, Raylib.RED);
+            if (RayGui.GuiButton(new Rectangle(window_width / 2 - 50, 200, 100, 50), "Play"))
+            {
+                state = GameState.Play;
+            }
+            if (RayGui.GuiButton(new Rectangle(window_width / 2 - 50, 300, 100, 50), "Options"))
+            {
+                state = GameState.Options;
+            }
+        }
+        void DevMenu()
+        {
+            Raylib.DrawText("Developer tools", window_width / 2 - 185, 30, 50, Raylib.RED);
+            if (RayGui.GuiButton(new Rectangle(window_width / 2 - 50, 200, 100, 50), "Play"))
+            {
+                state = GameState.Play;
+            }
+        }
+        void OptionsMenu()
+        {
+            Raylib.DrawText("Options", window_width / 2 - 85, 30, 50, Raylib.RED);
+            if (RayGui.GuiButton(new Rectangle(window_width / 2 - 50, 200, 100, 50), "Play"))
+            {
+                state = GameState.Play;
+            }
         }
     }
 }
